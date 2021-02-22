f31d14bfec512e9eb51c147d160c98e566277f18
parinyad123 <sci.equ@gmail.com>
2021-02-22 12:18:52 +0700

    values

---
+++ 02_Values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go"+"lang")
+
+	fmt.Println("1+1", 1+1)
+	fmt.Println("7.5+6.4", 7.5+6.4)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+    fmt.Println(!true)
+}
